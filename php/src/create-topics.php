3605ca94572d5f62441ae0a3198c16dcb3d386d7
Bazilio <4119114+xBazilio@users.noreply.github.com>
2021-05-21 23:24:35 +0300

    kafka_cluster php

---
+++ php/src/create-topics.php
@@ -0,0 +1,32 @@
+<?php
+
+$topicsConfig = [
+    'messenger-messages' => [
+        'partitions' => 500,
+        'replication-factor' => 3
+    ],
+
+    'likes' => [
+        'partitions' => 10,
+        'replication-factor' => 3
+    ]
+];
+
+echo PHP_EOL;
+echo 'Sadly, you can only create/delete topics with kafka bin/kafka-topics.sh util. 
+You need to connect to kafka-server to do it. 
+So here are commands to run:' . PHP_EOL . PHP_EOL;
+
+foreach ($topicsConfig as $name => $params) {
+    echo "\t";
+    // a bit of paranoia here
+    $name = escapeshellarg($name);
+    foreach ($params as $k => $v) {
+        $params[$k] = escapeshellarg($v);
+    }
+    echo "./bin/kafka-topics.sh --create --if-not-exists --topic {$name} "
+        . "--replication-factor {$params['replication-factor']} --partitions {$params['partitions']} "
+        . "--bootstrap-server localhost:9092";
+
+    echo PHP_EOL . PHP_EOL;
+}
