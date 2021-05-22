1f7d413bae14ce944e83082b57267d6956a6569c
Bazilio <4119114+xBazilio@users.noreply.github.com>
2021-05-22 18:26:36 +0300

    kafka_cluster

---
+++ php/src/consumer-likes.php
@@ -0,0 +1,36 @@
+<?php
+
+$conf = new RdKafka\Conf();
+$conf->set('metadata.broker.list', 'kafka1:9092,kafka2:9092,kafka3:9092');
+$conf->set('group.id', 'likes-consumer');
+$conf->set('auto.offset.reset', 'earliest');
+$conf->set('session.timeout.ms', 60000);
+$conf->set('group.instance.id', gethostname());
+
+$consumer = new RdKafka\KafkaConsumer($conf);
+
+$consumer->subscribe(['likes']);
+
+try {
+    while (true) {
+        $message = $consumer->consume(120 * 1000);
+        switch ($message->err) {
+            case RD_KAFKA_RESP_ERR_NO_ERROR:
+                $data = json_decode($message->payload, true);
+                echo "Got " . ($data['like'] ? 'like 👍' : 'dislike 👎') . " for Post #{$data['postId']}" . PHP_EOL;
+                break;
+            case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                echo "No more messages; will wait for more ⏳" . PHP_EOL;
+                break;
+            case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                echo "⌛ Timed out" . PHP_EOL;
+                break;
+            default:
+                echo "💩 Something went wrong" . PHP_EOL;
+                throw new \Exception($message->errstr(), $message->err);
+        }
+    }
+} finally {
+    $consumer->close();
+}
+
---
+++ php/src/producer-likes.php
@@ -0,0 +1,53 @@
+<?php
+
+$conf = new RdKafka\Conf();
+$conf->set('metadata.broker.list', 'kafka1:9092,kafka2:9092,kafka3:9092');
+
+class KafkaProducer
+{
+    private \RdKafka\Conf $conf;
+
+    public function __construct(RdKafka\Conf $conf)
+    {
+        $this->conf = $conf;
+    }
+
+    private function flush(\RdKafka\Producer $producer): bool
+    {
+        $result = RD_KAFKA_RESP_ERR_UNKNOWN;
+        for ($flushRetries = 0; $flushRetries < 10; $flushRetries++) {
+            $result = $producer->flush(10000);
+            if (RD_KAFKA_RESP_ERR_NO_ERROR === $result) {
+                break;
+            }
+        }
+        return RD_KAFKA_RESP_ERR_NO_ERROR === $result;
+    }
+
+    public function procuce($topicName, $message, $key): bool
+    {
+        $producer = new RdKafka\Producer($this->conf);
+
+        $topic = $producer->newTopic($topicName);
+
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, $message, $key);
+        $producer->poll(0);
+
+        return $this->flush($producer);
+    }
+}
+
+$producer = new KafkaProducer($conf);
+
+$postId = rand(35, 147);
+$like = rand(0, 5) > 1;
+
+$message = json_encode(['postId' => $postId, 'like' => $like]);
+
+$result = $producer->procuce('likes', $message, $postId);
+
+if ($result) {
+    echo "Post #{$postId} was " . ($like ? 'liked 👍' : 'disliked 👎') . PHP_EOL;
+} else {
+    echo "💩 couldn't produse message for Post #{$postId}" . PHP_EOL;
+}
